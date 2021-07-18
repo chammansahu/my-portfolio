9dacad7b8e1a7cf4597dd54f6cc66af98df49373
chammansahu <chammansahu@gmail.com>
2021-07-18 22:10:52 +0530

    Added Projects section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const about = document.querySelector('#about')
 const contact = document.querySelector('#contact')
+const projects = document.querySelector('#projects')
 const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
+const projectsContent = document.querySelector('#projects-content')
 
 about.addEventListener('click', () => {
   const aboutBox = new WinBox({
@@ -32,6 +34,24 @@ contact.addEventListener("click", () => {
     bottom: 50,
     left: 250,
     mount: contactContent,
+    onfocus: function () {
+      this.setBackground("#00aa00");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    }
+  });
+});
+projects.addEventListener("click", () => {
+  const aboutBox = new WinBox({
+    title: "Projecyt Completed By Me",
+    width: "600px",
+    height: "800px",
+    top: 150,
+    right: 50,
+    bottom: 50,
+    left: 250,
+    mount: projectsContent,
     onfocus: function () {
       this.setBackground("#00aa00");
     },
